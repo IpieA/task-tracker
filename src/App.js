efef0ac26de63c3e5deb1ab7cac904df0f9be733
IpieA <animpuyeraymond@gmail.com>
2023-04-19 15:24:03 +0100

    Added a deleteTask & toggleReminder functionality

--- src/App.js
+++ src/App.js
@@ -7,24 +7,42 @@ const App = () => {
     {
       id: 1,
       text: "Doctor's appointment",
-      day: "April, 19th at 12.00PM"
+      day: "April, 19th at 12.00PM",
+      reminder: true
     },
     {
       id: 2,
       text: "Go get my letter of employment",
-      day: "April, 19th at 12.00PM"
+      day: "April, 19th at 12.00PM",
+      reminder: true
     },
     {
       id: 3,
       text: "Meeting at work",
-      day: "April, 19th at 12.00PM"
+      day: "April, 19th at 12.00PM",
+      reminder: true
     }
   ])
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter(task => task.id !== id))
+  }
+
+  //toggleRemind for task
+  const toggleReminder = (id) => {
+    setTasks(tasks.map(task => task.id === id ? {...task, reminder: !task.reminder} : task))
+    //   if(task.id === id){
+    //     task.reminder = !task.reminder;
+    //   }
+    // }))
+  }
+  
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No tasks left"}
     </div>
   );
 }
